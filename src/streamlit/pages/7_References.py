fa2d562c3c58d3502dace9ed55d46cfa5935e8c6
florentdayde <florentdayde489@gmail.com>
2024-06-12 00:10:44 +0200

    feat(st): add script 7 references

---
+++ src/streamlit/pages/7_References.py
@@ -0,0 +1,42 @@
+# References for bibliography
+# sources for images etc.
+
+import streamlit as st
+from functions.footer import add_footer
+
+
+st.set_page_config(
+    page_title="References",
+    layout="wide"
+)
+
+
+style = """
+<style>
+.title {
+  font-size: 24px;
+  font-weight: bold;
+  color: white;
+}
+hr {
+  border-top: 2px solid #FF5733;  # You can change the color code as needed
+}
+</style>
+"""
+
+st.title("References & Sources")
+
+
+references_md = """
+
+"""
+
+sources_md = """
+
+"""
+
+
+
+
+
+add_footer()
